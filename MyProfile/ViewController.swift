cb5105a500d2594f47953377a2180e4a7d6c14a2
JunSeobKim <wnstjq616@gmail.com>
2020-12-01 17:19:43 +0900

    Initial Commit

---
+++ MyProfile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyProfile
+//
+//  Created by 김준섭 on 2020/12/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
