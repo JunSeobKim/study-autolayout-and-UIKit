7d08584d067bb1d73e0115855fdb34e30db3127e
JunSeobKim <wnstjq616@gmail.com>
2020-12-02 12:52:40 +0900

    study autolayout and UIKit

--- MyProfile/ViewController.swift
+++ MyProfile/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var submit: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
